fe3bb2d9e1b13d03749b20a33884d0169da95847
jotavetech <ojoaogugel@gmail.com>
2024-07-14 15:29:25 -0300

    fix: payment form schema export

--- app/validators/schemas/payment-form-schema.ts
+++ app/validators/schemas/payment-form-schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export default paymentFormSchema = z.object({
+const paymentFormSchema = z.object({
   cardHolder: z
     .string()
     .min(3, { message: "O nome precisa conter mais de 3 caracteres." }),
@@ -15,3 +15,5 @@ export default paymentFormSchema = z.object({
     message: "O CVV precisa conter 3 caracteres numerais.",
   }),
 });
+
+export default paymentFormSchema;
