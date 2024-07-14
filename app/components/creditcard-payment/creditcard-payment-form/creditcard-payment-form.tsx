af48169a7c4ecc1c71ead8ede735fe70e285ac07
jotavetech <ojoaogugel@gmail.com>
2024-07-14 15:28:28 -0300

    refact: separate payment form schema from the form component

--- app/components/creditcard-payment/creditcard-payment-form/creditcard-payment-form.tsx
+++ app/components/creditcard-payment/creditcard-payment-form/creditcard-payment-form.tsx
@@ -7,8 +7,6 @@ import {
   SubmitButton,
 } from "./creditcard-payment-form.styled";
 
-import * as z from "zod";
-
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useRouter } from "next/navigation";
@@ -17,26 +15,12 @@ import { useState } from "react";
 
 import { InputField } from "../..";
 
+import paymentFormSchema from "@/app/validators/schemas/payment-form-schema";
+
 interface CreditCardPaymentFormProps {
   installments: number;
   price: number;
 }
-
-const paymentFormSchema = z.object({
-  cardHolder: z
-    .string()
-    .min(3, { message: "O nome precisa conter mais de 3 caracteres." }),
-  cpf: z.string().length(11, { message: "O CPF é inválido." }),
-  cardNumber: z.string().length(16, {
-    message: "Número inválido.",
-  }),
-  expirationDate: z.string().length(6, {
-    message: "Data de vencimento inválida.",
-  }),
-  cvv: z.string().length(3, {
-    message: "O CVV precisa conter 3 caracteres numerais.",
-  }),
-});
 
 export function CreditCardPaymentForm({
   installments,
---
+++ app/validators/schemas/payment-form-schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export default paymentFormSchema = z.object({
+  cardHolder: z
+    .string()
+    .min(3, { message: "O nome precisa conter mais de 3 caracteres." }),
+  cpf: z.string().length(11, { message: "O CPF é inválido." }),
+  cardNumber: z.string().length(16, {
+    message: "Número inválido.",
+  }),
+  expirationDate: z.string().length(6, {
+    message: "Data de vencimento inválida.",
+  }),
+  cvv: z.string().length(3, {
+    message: "O CVV precisa conter 3 caracteres numerais.",
+  }),
+});
